e511d82ae81427b12d4e2581b64dc9fd52d8d8e5
Cameron Klein <cameron.klein@gmail.com>
2015-09-19 15:32:07 -0700

    Create Xcode project

---
+++ BigScreenGifs/BigScreenGifs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BigScreenGifs
+//
+//  Created by Cameron Klein on 9/19/15.
+//  Copyright © 2015 Cameron Klein. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
